1cf131604b15e3d1820ff21932bfb30db4544cd6
EkaterinaSukhanova <katrinsuh@yandex.ru>
2018-07-06 17:26:40 +0300

    task3 v1

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -6,6 +6,26 @@
  * @returns {{min:number, max:number}}  объект
  */
 function getMinMax(str) {
-
+    let num = '';
+    let result = {
+        max: -10000000,
+        min: 10000000
+    };
+    let index = 0;
+    for (let char of str) {
+        if ((char.charCodeAt(0) >= 45) && (char.charCodeAt(0) <= 57)){
+            num += char;
+            if((index + 1) === str.length){
+                if (+num > result.max) result.max = +num;
+                if (+num < result.min) result.min = +num;
+            }
+        } else {
+            if (+num > result.max) result.max = +num;
+            if (+num < result.min) result.min = +num;
+            num = '';
+        }
+        index ++;
+    }
+    return result;
 }
 
